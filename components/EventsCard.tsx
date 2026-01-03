fa51462557160d28b4f8cbab553ab4d19ac9ac68
Pallav Rai <pallavrai8953@gmail.com>
2026-01-03 20:31:58 +0530

    feat: add PostgreSQL support and update dependencies in configuration files

--- components/EventsCard.tsx
+++ components/EventsCard.tsx
@@ -14,7 +14,7 @@ const EventsCards = ({ eventsPromise }: EventsCardProps) => {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      {events.map((event, idx) => {
+      {events.map((event) => {
         return (
           <div
             key={event.id}
